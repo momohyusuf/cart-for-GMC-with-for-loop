db7329416a8a1062c4da6430ffddb121f3bd699f
momohyusuf <starkweb2@gmail.com>
2024-06-04 14:07:44 +0100

    Modified the shopping cart to use OOP approach

--- app.js
+++ app.js
@@ -2,69 +2,102 @@ const CART_ITEMS_CONTAINER = document.getElementById("cart__items__container");
 const DISPLAY_CART_TOTAL = document.getElementById("display__cart__total");
 const CHECK_OUT_BTN = document.getElementById("check__out__btn");
 
-let cartItems = [
-  {
-    product_id: 1,
-    product_name: "Samsung",
-    product_image:
-      "https://th.bing.com/th/id/R.00f0eb654b5f202b6156cd4f37437f78?rik=a3d6Tyo5aYH9TA&pid=ImgRaw&r=0",
-    product_price: 2000,
-    product_quantity: 1,
-  },
-  {
-    product_id: 2,
-    product_name: "Iphone 12",
-    product_image:
-      "https://th.bing.com/th/id/OIP.-22fs7W_R-MVukHjyGOq1AHaEx?rs=1&pid=ImgDetMain",
-    product_price: 5000,
-    product_quantity: 1,
-  },
-  {
-    product_id: 3,
-    product_name: "Tecno Pop 2",
-    product_image:
-      "https://th.bing.com/th/id/OIP.b8RtZPhsoqNnFbpT0eZTXAHaHa?rs=1&pid=ImgDetMain",
-    product_price: 10000,
-    product_quantity: 1,
-  },
-  {
-    product_id: 4,
-    product_name: "Infinix hot 5",
-    product_image:
-      "https://th.bing.com/th/id/OIP.Nuvc-slbqHCH0l169HCt_AHaHa?rs=1&pid=ImgDetMain",
-    product_price: 50000,
-    product_quantity: 1,
-  },
-  {
-    product_id: 5,
-    product_name: "Oppo A5",
-    product_image:
-      "https://th.bing.com/th/id/R.b4b12bd39f278242c3a1201fce360bb2?rik=idzKC3GG3bCEcQ&pid=ImgRaw&r=0",
-    product_price: 25000,
-    product_quantity: 1,
-  },
-];
+// A class to create product instance
+class Product {
+  constructor(
+    product_id,
+    product_name,
+    product_image,
+    product_price,
+    product_quantity
+  ) {
+    this.product_id = product_id;
+    this.product_name = product_name;
+    this.product_image = product_image;
+    this.product_price = product_price;
+    this.product_quantity = product_quantity;
+  }
+}
 
-// a function to display cart items
+// An instance of the shopping cart
+class ShoppingCartItems {
+  constructor(cartItems) {
+    this.cartItems = cartItems;
+  }
 
-function display_cart_items() {
-  let product_to_display = [];
+  // A method to increase product quantity
+  increaseProductQuantity(productID) {
+    for (let i = 0; i < this.cartItems.length; i++) {
+      // check for the product that was click
+      if (this.cartItems[i].product_id === productID) {
+        this.cartItems[i].product_quantity++;
+      }
+    }
+    this.calculateCartTotal();
+    this.display_cart_items();
+  }
+  // ******increase product quantity method ends here
 
-  for (let i = 0; i < cartItems.length; i++) {
-    const cart_product = ` <!-- single cart product -->
+  // A method  to decrease product quantity
+  decreaseProductQuantity(productID) {
+    for (let i = 0; i < this.cartItems.length; i++) {
+      if (
+        this.cartItems[i].product_id === productID &&
+        this.cartItems[i].product_quantity != 1
+      ) {
+        this.cartItems[i].product_quantity--;
+      }
+    }
+    this.calculateCartTotal();
+    this.display_cart_items();
+  }
+  // ******* decrease product quantity ends here
+
+  // A method for removing item from cart
+  removeItemFromCart(productID) {
+    const productsLeftInCart = [];
+    for (let i = 0; i < this.cartItems.length; i++) {
+      if (this.cartItems[i].product_id !== productID) {
+        productsLeftInCart.push(this.cartItems[i]);
+      }
+    }
+    this.cartItems = productsLeftInCart;
+    this.display_cart_items();
+    this.calculateCartTotal();
+  }
+  //******* remove item from cart method ends here
+
+  // A method to calculate the total amount of products
+  calculateCartTotal() {
+    let totalCost = 0;
+    for (let i = 0; i < this.cartItems.length; i++) {
+      totalCost =
+        totalCost +
+        this.cartItems[i].product_price * this.cartItems[i].product_quantity;
+      DISPLAY_CART_TOTAL.textContent = totalCost;
+    }
+    return totalCost;
+  }
+  // calculate cart total items ends here
+
+  // A method to display cart items
+  display_cart_items() {
+    let product_to_display = [];
+
+    for (let i = 0; i < this.cartItems.length; i++) {
+      const cart_product = ` <!-- single cart product -->
           <div class="flex justify-between items-center shadow-md p-5">
             <div class="flex items-center gap-4">
               <img
-                src=${cartItems[i].product_image}
+                src=${this.cartItems[i].product_image}
                 alt="product image"
                 class="w-40 rounded-md"
               /> 
               <div>
-                <h2 class="font-bold text-3xl">${cartItems[i].product_name}</h2>
+                <h2 class="font-bold text-3xl">${this.cartItems[i].product_name}</h2>
                 <button
-                  class="bg-red-500 text-white font-semibold p-2 rounded-md mt-2"
-
-                   onclick="removeItemFromCart(${cartItems[i].product_id})"
+                  class="delete__item bg-red-500 text-white font-semibold p-2 rounded-md mt-2"
+                  id=${this.cartItems[i].product_id}
                 >
                   Delete
                 </button>
@@ -72,17 +105,17 @@ function display_cart_items() {
             </div>
 
             <div class="text-center">
-              <p class="font-bold text-xl">${cartItems[i].product_price}</p>
+              <p class="font-bold text-xl">${this.cartItems[i].product_price}</p>
               <button
-                class="bg-green-500 text-white text-lg font-semibold p-2 rounded-md mt-2"
-                onclick="increaseProductQuantity(${cartItems[i].product_id})"
-              >
+                class="increase__product__quantity bg-green-500 text-white text-lg font-semibold p-2 rounded-md mt-2"
+                 id=${this.cartItems[i].product_id}  
+                >
                 +
               </button>
-              <span class="font-bold text-lg">${cartItems[i].product_quantity}</span>
+              <span class="font-bold text-lg">${this.cartItems[i].product_quantity}</span>
               <button
-                class="bg-red-500 text-white text-lg font-semibold p-2 rounded-md mt-2"
-                onclick="decreaseProductQuantity(${cartItems[i].product_id})"
+                class="decrease__product__quantity bg-red-500 text-white text-lg font-semibold p-2 rounded-md mt-2"
+                id=${this.cartItems[i].product_id} 
               >
                 -
               </button>
@@ -90,73 +123,94 @@ function display_cart_items() {
           </div>
           <!-- single cart product ends here -->`;
 
-    product_to_display.push(cart_product);
-  }
-
-  if (product_to_display.length === 0) {
-    CART_ITEMS_CONTAINER.innerHTML = `<h1 class="text-center text-3xl font-semibold">Cart is empty please add product 🥲🥲</h1>`;
-    // DISPLAY_CART_TOTAL.textContent = 0;
-    return;
-  }
-
-  CART_ITEMS_CONTAINER.innerHTML = product_to_display.join(" ");
-}
-display_cart_items();
-calculateCartTotal();
-
-// function to increase product quantity
-function increaseProductQuantity(productID) {
-  for (let i = 0; i < cartItems.length; i++) {
-    // check for the product that was click
-    if (cartItems[i].product_id === productID) {
-      cartItems[i].product_quantity++;
+      product_to_display.push(cart_product);
     }
-  }
 
-  calculateCartTotal();
-  display_cart_items();
-}
-
-// function to decrease product quantity
-function decreaseProductQuantity(productID) {
-  for (let i = 0; i < cartItems.length; i++) {
-    if (
-      cartItems[i].product_id === productID &&
-      cartItems[i].product_quantity != 1
-    ) {
-      cartItems[i].product_quantity--;
+    if (product_to_display.length === 0) {
+      CART_ITEMS_CONTAINER.innerHTML = `<h1 class="text-center text-3xl font-semibold">Cart is empty please add product 🥲🥲</h1>`;
+      // DISPLAY_CART_TOTAL.textContent = 0;
+      return;
     }
+
+    CART_ITEMS_CONTAINER.innerHTML = product_to_display.join(" ");
+    // increase quantity button
+    const increaseProductQuantityBTN = document.querySelectorAll(
+      ".increase__product__quantity"
+    );
+    increaseProductQuantityBTN.forEach((btn) => {
+      btn.addEventListener("click", (event) => {
+        let productID = parseInt(event.target.getAttribute("id"));
+        this.increaseProductQuantity(productID);
+      });
+    });
+    // increase quantity button ends here
+
+    // decrease quantity button starts here
+    const decreaseProductQuantityBTN = document.querySelectorAll(
+      ".decrease__product__quantity"
+    );
+    decreaseProductQuantityBTN.forEach((btn) => {
+      btn.addEventListener("click", (event) => {
+        let productID = parseInt(event.target.getAttribute("id"));
+        this.decreaseProductQuantity(productID);
+      });
+    });
+    // decrease quantity button ends here
+
+    // delete item  button starts here
+    const deleteItemBTN = document.querySelectorAll(".delete__item");
+    deleteItemBTN.forEach((btn) => {
+      btn.addEventListener("click", (event) => {
+        let productID = parseInt(event.target.getAttribute("id"));
+        this.removeItemFromCart(productID);
+      });
+    });
+    // decrease quantity button ends here
   }
-  calculateCartTotal();
-  display_cart_items();
+  // ******display cart items method ends her
 }
 
-// function to remove product from cart
-function removeItemFromCart(productID) {
-  const productsLeftInCart = [];
-  for (let i = 0; i < cartItems.length; i++) {
-    if (cartItems[i].product_id !== productID) {
-      productsLeftInCart.push(cartItems[i]);
-    }
-  }
-  cartItems = productsLeftInCart;
-  display_cart_items();
-  calculateCartTotal();
-}
+let cartItems = [
+  new Product(
+    1,
+    "Samsung",
+    "https://th.bing.com/th/id/R.00f0eb654b5f202b6156cd4f37437f78?rik=a3d6Tyo5aYH9TA&pid=ImgRaw&r=0",
+    20000,
+    1
+  ),
 
-// function to calculate cart total
-function calculateCartTotal() {
-  let totalCost = 0;
-  for (let i = 0; i < cartItems.length; i++) {
-    totalCost =
-      totalCost + cartItems[i].product_price * cartItems[i].product_quantity;
-    DISPLAY_CART_TOTAL.textContent = totalCost;
-  }
-  return totalCost;
-}
+  new Product(
+    2,
+    "Iphone 12",
+    "https://th.bing.com/th/id/R.00f0eb654b5f202b6156cd4f37437f78?rik=a3d6Tyo5aYH9TA&pid=ImgRaw&r=0",
+    5999,
+    1
+  ),
+
+  new Product(
+    3,
+    "Infinix hot 5",
+    "https://th.bing.com/th/id/OIP.Nuvc-slbqHCH0l169HCt_AHaHa?rs=1&pid=ImgDetMain",
+    50000,
+    1
+  ),
+
+  new Product(
+    4,
+    "Tecno Camon 15",
+    "https://th.bing.com/th/id/OIP.1Y8zv6J6o4Q2qQr9J1QG4wHaHa?pid=ImgDet&rs=1",
+    30000,
+    1
+  ),
+];
+
+// create an instance of the shopping cart
+const CustomerShoppingCart = new ShoppingCartItems(cartItems);
+CustomerShoppingCart.display_cart_items();
+CustomerShoppingCart.calculateCartTotal();
 
 CHECK_OUT_BTN.addEventListener("click", handleCheckOut);
 function handleCheckOut() {
-  console.log(cartItems);
-  console.log(calculateCartTotal());
+  console.log(CustomerShoppingCart.cartItems);
+  console.log(CustomerShoppingCart.calculateCartTotal());
 }
